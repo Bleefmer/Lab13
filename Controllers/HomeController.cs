81de701a45f922fa9ffe6f7b4cb4e98aa5c4b355
Bleefmer <118676325+Bleefmer@users.noreply.github.com>
2024-11-24 17:15:09 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        Log.Debug("This is a debug message.");
+        Log.Information("This is an information message.");
+        Log.Warning("This is a warning message.");
+        Log.Error("This is an error message.");
+        Log.Fatal("This is a fatal error message.");
+
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNetCoreSerilogProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
